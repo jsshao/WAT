f9b426d559fb5be359af5a286cae9c8eb0439dd1
Eddy Gao <shademaster00@gmail.com>
2015-10-08 01:47:53 -0400

    create basic extension structure and begin work on capture audio

--- chrome/background.js
+++ chrome/background.js
@@ -1,5 +1,26 @@
-chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.tabs.create({
-    url: "http://mangareader.net"
-  });
-});
+(function() {
+    var activeTab = -1;
+    var audioStream = null;
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        chrome.tabCapture.getCapturedTabs(function (tabs) {
+            if (audioStream != null) {
+                console.log("stopping capture");
+                var tracks = audioStream.getTracks();
+                tracks.forEach(function(track) {
+                    track.stop();
+                });
+                chrome.browserAction.setIcon({path:"icon.png"});
+                audioStream = null;
+                if (activeTab == tab.id) {
+                    activeTab = -1;
+                    return;   
+                }
+            };
+            console.log("capturing");
+            chrome.tabCapture.capture({'audio':true, 'video':false}, function(stream) {
+                audioStream = stream;
+                chrome.browserAction.setIcon({path:"playing.png"});
+                activeTab = tab.id;
+            });
+        });
+    })})();
